13934f398f76eaade2b669a8dbeb6d9837cecc81
alex-fung <alexcfung@gmail.com>
2014-11-10 01:35:33 -0500

    Created iOS project

---
+++ bolin-time/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bolin-time
+//
+//  Created by alexfung on 2014-11-10.
+//  Copyright (c) 2014 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
