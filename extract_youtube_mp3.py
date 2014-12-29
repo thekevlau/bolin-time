b3623cf6a088daeef54d473a9b40fc5cbee3cfb8
Kevin Lau <thekevlau@gmail.com>
2014-12-29 18:30:43 -0500

    added script for youtube/mp3 conversion to be moved to server, removed unnecessary youtube-dl source

--- extract_youtube_mp3.py
+++ extract_youtube_mp3.py
@@ -4,8 +4,11 @@ import subprocess
 
 __all__=('extract_youtube_mp3')
 
+# uses 'youtube-dl' python script to extract an mp3 format audio file of a given youtube video 
+
 def extract_youtube_mp3(youtube_url=None):
-    subprocess.call("youtube-dl -s --audio-format mp3 %s" % (youtube_url))
+    assemble_cmd = "youtube-dl -x --audio-format mp3 %s" % (youtube_url)
+    subprocess.call(assemble_cmd, shell=True)
 
 if __name__ == "__main__":
     youtube_url = raw_input("Please enter the url of the youtube video: ")
