730eec1c5148ca9301d14fae5f5129eb639d5178
Kevin Lau <thekevlau@gmail.com>
2014-12-29 15:36:57 -0500

    some changes

--- bolin-time/AppDelegate.swift
+++ bolin-time/AppDelegate.swift
@@ -26,7 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         var managedObjectContext = NSManagedObjectContext()
         managedObjectContext.persistentStoreCoordinator = coordinator
         return managedObjectContext
-    } ()
+    }()
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
--- bolin-time/ViewController.swift
+++ bolin-time/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        println(managedObjectContext!)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
