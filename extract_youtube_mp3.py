7ab6bdd626a3a30fdf455f49326f3bebfecb4213
Kevin Lau <thekevlau@gmail.com>
2014-12-29 22:37:50 -0500

    finalized youtube extraction; still need to figure out how to automate brew installation stuff

--- extract_youtube_mp3.py
+++ extract_youtube_mp3.py
@@ -1,10 +1,11 @@
-import os
 import string
 import subprocess
 
 __all__=('extract_youtube_mp3')
 
 # uses 'youtube-dl' python script to extract an mp3 format audio file of a given youtube video 
+# requires brew install ffmpeg
+# requires brew install youtube-dl
 
 def extract_youtube_mp3(youtube_url=None):
     assemble_cmd = "youtube-dl -x --audio-format mp3 %s" % (youtube_url)
@@ -13,5 +14,3 @@ def extract_youtube_mp3(youtube_url=None):
 if __name__ == "__main__":
     youtube_url = raw_input("Please enter the url of the youtube video: ")
     extract_youtube_mp3(youtube_url)
-
-
